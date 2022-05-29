00f4dad69befd5810c14270af2cc4e02a6e405ad
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-29 18:42:56 +0000

    Sun, May 29, 2022, 6:42 PM +00:00

--- routes/index.js
+++ routes/index.js
@@ -66,4 +66,24 @@ router.post('/addEvent', function(req, res, next) {
   });
 })
 
+/* POST update email preference. */
+router.post('/updateEmailPreference', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "UPDATE Email_preference SET  user_respond = ?";
+    connection.query(query, [req.body.username, req.body.password], function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows); //send response
+    });
+  });
+})
+
 module.exports = router;
