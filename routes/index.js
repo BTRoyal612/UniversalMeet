18bf48d8dee8d92e44ad51f2a221af468dd933ad
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-29 18:14:22 +0000

    Sun, May 29, 2022, 6:14 PM +00:00

--- routes/index.js
+++ routes/index.js
@@ -29,34 +29,42 @@ router.post('/username', function(req, res, next) {
 /* GET user, update password. */
 router.post('/updatePassword', function(req, res, next) {
   // Connect to the database
+  var id;
   req.pool.getConnection(function(err, connection) {
-
     if (err) {
       res.sendStatus(500);
       return;
     }
-
     var query = "SELECT user_id FROM User WHERE username = ? AND password = ?";
     connection.query(query, [req.body.username, req.body.currentPass], function(err, rows, fields) {
       connection.release(); // release connection
       if (err) {
-
         res.sendStatus(500);
         return;
       }
-      console.log(rows[0].user_id);
-      var query = "UPDATE User SET password = ? WHERE user_id = ?";
-      connection.query(query, [req.body.newPass, rows[0].user_id], function(err, rows, fields) {
-        connection.release(); // release connection
-        if (err) {
-          res.sendStatus(500);
-          return;
-        }
-      });
-
-      res.json(rows); //send response
+      id = rows;
     });
   });
+
+  console.log(id);
+
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "UPDATE User SET password = ? WHERE user_id = ?";
+    connection.query(query, [req.body.newPass, id[0].user_id], function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      console.log(rows);
+    });
+  });
+
+  res.json(id); //send response
 })
 
 module.exports = router;
