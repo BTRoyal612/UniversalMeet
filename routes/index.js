afda68c2b0011d106e9ae1b917911952315abb84
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-29 14:43:34 +0000

    Sun, May 29, 2022, 2:43 PM +00:00

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET user. */
+router.get('/getUser', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "SELECT first_name, last_name FROM actor";
+    connection.query(query, function(err, rows, fields) {
+    connection.release(); // release connection
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    res.json(rows); //send response
+    });
+  });
+})
+
 module.exports = router;
