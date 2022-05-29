4eb00e5e52a79da50ba5cbb8346b1e506704f1d9
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-29 15:38:00 +0000

    Sun, May 29, 2022, 3:38 PM +00:00

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,24 @@ router.get('/username', function(req, res, next) {
   });
 })
 
-
+/* GET user. */
+router.get('/updatePassword', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "SELECT user_id FROM User WHERE username = ? AND password = ?";
+    connection.query(query, [req.body.username, req.body.password], function(err, rows, fields) {
+    connection.release(); // release connection
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    res.json(rows); //send response
+    });
+  });
+})
 
 module.exports = router;
