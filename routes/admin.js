a71fa9053dc5dd4d322224b064c7257e2e178286
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-30 18:09:15 +0000

    Mon, May 30, 2022, 6:09 PM +00:00

--- routes/admin.js
+++ routes/admin.js
@@ -41,6 +41,26 @@ router.post('/getUserList', function(req, res, next) {
   });
 })
 
+/* POST add admin. */
+router.post('/addUser', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "INSERT INTO User (username, password) VALUES (?, ?)";
+    connection.query(query, [req.body.username, req.body.password], function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.send(); //send response
+    });
+  });
+})
+
 /* POST delete user admin. */
 router.post('/deleteUser', function(req, res, next) {
   // Connect to the database
@@ -77,6 +97,66 @@ router.post('/getEventList', function(req, res, next) {
         return;
       }
       res.json(rows); //send response
+    });
+  });
+})
+
+/* POST delete event. */
+router.post('/deleteEvent', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "DELETE FROM Event WHERE event_id = ?";
+    connection.query(query, [req.body.event_id],function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.send(); //send response
+    });
+  });
+})
+
+/* POST update event. */
+router.post('/updateEvent', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "UPDATE Event SET event_name = ?, duration = ?, time_zone = ?, hold_location = ?, due_date = ?, note = ?, share_link = ?, isOnline = ? WHERE event_id = ?";
+    connection.query(query, [req.body.event_name, req.body.duration, req.body.time_zone, req.body.hold_location, req.body.due_date, req.body.note, req.body.share_link, req.body.isOnline, req.body.event_id], function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.send(); //send response
+    });
+  });
+})
+
+/* POST update user email preference. */
+router.post('/updateEmailPreference', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "UPDATE Email_preference SET user_respond = ?, avail_confirm = ?, event_finalize = ?, event_cancel = ? WHERE user_id = ?";
+    connection.query(query, [req.body.user_respond, req.body.avail_confirm, req.body.event_finalize, req.body.event_cancel, req.body.user_id], function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.send(); //send response
     });
   });
 })
