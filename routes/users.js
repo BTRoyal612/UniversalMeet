5ce6e1827f3ae1d304f9133697f66fc7d828d1f9
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-30 08:54:03 +0000

    Mon, May 30, 2022, 8:54 AM +00:00

--- routes/users.js
+++ routes/users.js
@@ -114,8 +114,8 @@ router.post('/updateEvent', function(req, res, next) {
       res.sendStatus(500);
       return;
     }
-    var query = "UPDATE Event SET user_respond = ?, avail_confirm = ?, event_finalize = ?, event_cancel = ? WHERE user_id = ?";
-    connection.query(query, [req.body.user_respond, req.body.avail_confirm, req.body.event_finalize, req.body.event_cancel, req.body.user_id], function(err, rows, fields) {
+    var query = "UPDATE Event SET event_name = ?, duration = ?, time_zone = ?, hold_location = ?, due_date = ?, note = ?, share_link = ?, isOnline = ? WHERE event_id = ?";
+    connection.query(query, [req.body.event_name, req.body.duration, req.body.time_zone, req.body.hold_location, req.body.due_date, req.body.note, req.body.share_link, req.body.isOnline, req.body.event_id], function(err, rows, fields) {
       connection.release(); // release connection
       if (err) {
         res.sendStatus(500);
@@ -125,7 +125,6 @@ router.post('/updateEvent', function(req, res, next) {
     });
   });
 })
-
 
 /* POST update email preference. */
 router.post('/updateEmailPreference', function(req, res, next) {
