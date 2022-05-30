7da2f0b1fe55235ca56a3aca91149db5e8bc1497
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-30 08:28:42 +0000

    Mon, May 30, 2022, 8:28 AM +00:00

--- routes/admin.js
+++ routes/admin.js
@@ -3,60 +3,60 @@ var router = express.Router();
 
 /* POST add admin. */
 router.post('/addAdmin', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "INSERT INTO User (username, password, isAdmin) VALUES (?, ?, true)";
+    connection.query(query, [req.body.username, req.body.password], function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.send(); //send response
+    });
+  });
+})
+
+/* POST get user list admin. */
+router.post('/getUserList', function(req, res, next) {
+  // Connect to the database
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "SELECT * FROM User";
+    connection.query(query, function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows); //send response
+    });
+  });
+})
+
+/* POST delete user admin. */
+router.post('/deleteUser', function(req, res, next) {
     // Connect to the database
     req.pool.getConnection(function(err, connection) {
       if (err) {
         res.sendStatus(500);
         return;
       }
-      var query = "INSERT INTO User (username, password, isAdmin) VALUES (?, ?, true)";
-      connection.query(query, [req.body.username, req.body.password], function(err, rows, fields) {
+      var query = "DELETE FROM User WHERE user_id = ?";
+      connection.query(query, [req.body.user_id], function(err, rows, fields) {
         connection.release(); // release connection
         if (err) {
           res.sendStatus(500);
           return;
         }
         res.send(); //send response
-      });
-    });
-  })
-
-  /* POST get user list admin. */
-  router.post('/getUserList', function(req, res, next) {
-    // Connect to the database
-    req.pool.getConnection(function(err, connection) {
-      if (err) {
-        res.sendStatus(500);
-        return;
-      }
-      var query = "SELECT * FROM User";
-      connection.query(query, function(err, rows, fields) {
-        connection.release(); // release connection
-        if (err) {
-          res.sendStatus(500);
-          return;
-        }
-        res.json(rows); //send response
-      });
-    });
-  })
-
-  /* POST get user list admin. */
-router.post('/getUserListAdmin', function(req, res, next) {
-    // Connect to the database
-    req.pool.getConnection(function(err, connection) {
-      if (err) {
-        res.sendStatus(500);
-        return;
-      }
-      var query = "SELECT * FROM User";
-      connection.query(query, function(err, rows, fields) {
-        connection.release(); // release connection
-        if (err) {
-          res.sendStatus(500);
-          return;
-        }
-        res.json(rows); //send response
       });
     });
   })
