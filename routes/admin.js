f34350fdcc81f042c418a598d5e548e06906e30a
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-30 08:23:21 +0000

    Mon, May 30, 2022, 8:23 AM +00:00

---
+++ routes/admin.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST add admin. */
+router.post('/addAdmin', function(req, res, next) {
+    // Connect to the database
+    req.pool.getConnection(function(err, connection) {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      var query = "INSERT INTO User (username, password, isAdmin) VALUES (?, ?, true)";
+      connection.query(query, [req.body.username, req.body.password], function(err, rows, fields) {
+        connection.release(); // release connection
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+        res.send(); //send response
+      });
+    });
+  })
+
+  /* POST get user list admin. */
+  router.post('/getUserList', function(req, res, next) {
+    // Connect to the database
+    req.pool.getConnection(function(err, connection) {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      var query = "SELECT * FROM User";
+      connection.query(query, function(err, rows, fields) {
+        connection.release(); // release connection
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+        res.json(rows); //send response
+      });
+    });
+  })
+
+module.exports = router;
