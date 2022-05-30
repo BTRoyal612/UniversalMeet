67ac33e135b28b6cda7bd7d5cde248c22eef63f5
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-05-30 14:30:33 +0000

    Mon, May 30, 2022, 2:30 PM +00:00

--- routes/users.js
+++ routes/users.js
@@ -6,46 +6,6 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* POST get user. */
-router.post('/getUser', function(req, res, next) {
-  // Connect to the database
-  req.pool.getConnection(function(err, connection) {
-    if (err) {
-      res.sendStatus(500);
-      return;
-    }
-    var query = "SELECT user_id FROM User WHERE username = ? AND password = ?";
-    connection.query(query, [req.body.username, req.body.password], function(err, rows, fields) {
-      connection.release(); // release connection
-      if (err) {
-        res.sendStatus(500);
-        return;
-      }
-      res.json(rows); //send response
-    });
-  });
-})
-
-/* POST add user. */
-router.post('/addUser', function(req, res, next) {
-  // Connect to the database
-  req.pool.getConnection(function(err, connection) {
-    if (err) {
-      res.sendStatus(500);
-      return;
-    }
-    var query = "INSERT INTO User (username, password) VALUES (?, ?)";
-    connection.query(query, [req.body.username, req.body.passwordd], function(err, rows, fields) {
-      connection.release(); // release connection
-      if (err) {
-        res.sendStatus(500);
-        return;
-      }
-      res.send(); //send response
-    });
-  });
-})
-
 /* POST get event. */
 router.post('/getEvent', function(req, res, next) {
   // Connect to the database
