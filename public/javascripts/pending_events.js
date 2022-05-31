395aed06ad6ea10f35fb419fd255766e3d008f85
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-05-31 06:16:41 +0000

    Tue, May 31, 2022, 6:16 AM +00:00

--- public/javascripts/pending_events.js
+++ public/javascripts/pending_events.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
 
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        console.log(vueinst.p_event);
         Vue.set(vueinst.p_event, JSON.parse(this.responseText))
       }
   }
