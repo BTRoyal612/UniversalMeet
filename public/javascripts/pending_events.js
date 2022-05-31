2e4be5335c92bad4b251837febb3ffda2880ff41
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-05-31 06:09:27 +0000

    Tue, May 31, 2022, 6:09 AM +00:00

--- public/javascripts/pending_events.js
+++ public/javascripts/pending_events.js
@@ -41,24 +41,24 @@ const PENDING_EVENTS = [
   },
 ]
 
-function getUserEventList() {
-  console.log("pass event list");
-  var xhttp = new XMLHttpRequest();
-
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        PENDING_EVENTS = JSON.parse(this.responseText);
-        console.log(PENDING_EVENTS);
-      }
-  }
-
-  xhttp.open("POST", "/users/getEventList", true);
-  xhttp.send();
-};
-
 var vueinst = new Vue({
   el: "#app",
   data: {
       p_event: PENDING_EVENTS,
   }
 });
+
+
+function getUserEventList() {
+  console.log("pass event list");
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        Vue.set(vueinst.p_event, JSON.parse(this.responseText))
+      }
+  }
+
+  xhttp.open("POST", "/users/getEventList", true);
+  xhttp.send();
+};
