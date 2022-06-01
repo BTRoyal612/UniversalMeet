ef8f7d5586aa3be623072d2c9f7081f72e2b2b84
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-06-01 13:16:51 +0000

    Wed, Jun 1, 2022, 1:16 PM +00:00

--- public/javascripts/pending_events.js
+++ public/javascripts/pending_events.js
@@ -4,21 +4,21 @@ $(document).ready(function () {
   console.log("pass event list");
   var xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        vueinst.p_event.splice(0);
-        console.log(JSON.parse(this.responseText))
-        var events = JSON.parse(this.responseText);
-        for (let event of events) {
-          var isHost;
-          if (event.creator_id == event.user_id) isHost = true;
-          else isHost = false;
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      vueinst.p_event.splice(0);
+      console.log(JSON.parse(this.responseText))
+      var events = JSON.parse(this.responseText);
+      for (let event of events) {
+        var isHost;
+        if (event.creator_id == event.user_id) isHost = true;
+        else isHost = false;
 
-          event["isHost"] = isHost;
-          console.log(event);
-          vueinst.p_event.push(event);
-        }
+        event["isHost"] = isHost;
+        console.log(event);
+        vueinst.p_event.push(event);
       }
+    }
   }
 
   xhttp.open("POST", "/users/getEventList", true);
@@ -28,18 +28,18 @@ $(document).ready(function () {
 var vueinst = new Vue({
   el: "#app",
   data: {
-      p_event: PENDING_EVENTS,
+    p_event: PENDING_EVENTS,
   }
 });
 
 const getClickEvent = (event_id) => {
   var xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText)
-        }
-      }
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText)
+    }
+  }
 
   xhttp.open("POST", "/users/getEvent", true);
   xhttp.setRequestHeader("Content-type", "application/json");
