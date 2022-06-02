073ad0e3d5592ebee20a1e90d4f0c34c3f4149d4
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-06-02 12:48:22 +0000

    Thu, Jun 2, 2022, 12:48 PM +00:00

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -121,6 +121,7 @@ function passDate() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        window.location = './event-information.html'
       }
   }
 
