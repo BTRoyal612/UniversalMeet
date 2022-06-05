50b499ebf0c6b99de0c1810e2970fb36701f5549
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-06-05 14:22:24 +0000

    Sun, Jun 5, 2022, 2:22 PM +00:00

--- public/javascripts/availbility.js
+++ public/javascripts/availbility.js
@@ -29,6 +29,19 @@ function addAvailability(time_frame) {
       }
   }
 
+  function addEvent() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        }
+    }
+
+    xhttp.open("POST", "/users/addEvent", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({ event_name:<%=event_name%>, duration:event.duration, time_zone:event.time_zone, hold_location:event.hold_location, due_date:event.due_date, note:event.note, share_link:event.share_link, isOnline:event.isOnline }));
+  };
+
   xhttp.open("POST", "/users/addAvailability", true);
   xhttp.setRequestHeader("Content-type", "application/json");
   xhttp.send(JSON.stringify({ time_frame: time_frame}));
