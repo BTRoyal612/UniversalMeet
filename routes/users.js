14a9c97d15f756a4e46505c762a22d7036b8c550
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-06-05 06:15:53 +0000

    Sun, Jun 5, 2022, 6:15 AM +00:00

--- routes/users.js
+++ routes/users.js
@@ -122,7 +122,7 @@ router.post('/addEvent', function(req, res, next) {
         res.sendStatus(500);
         return;
       }
-      // console.log(rows[0]);
+      console.log(rows[0][0].event_id);
       // event_id = rows[0][0].event_id;
       res.json(rows); //send response
     });
