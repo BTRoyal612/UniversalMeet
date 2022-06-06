9293a76398653997161741e14cf94f55a6fff81a
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-06-06 13:41:19 +0000

    Mon, Jun 6, 2022, 1:41 PM +00:00

--- public/javascripts/event-information.js
+++ public/javascripts/event-information.js
@@ -35,7 +35,8 @@ function addEvent() {
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         id_input = document.getElementById('event_id');
-        id_input.value = JSON.parse(this.responseText);
+        id_input.value = JSON.parse(this.responseText)[0];
+        console.log(JSON.parse(this.responseText))
       }
   }
 
