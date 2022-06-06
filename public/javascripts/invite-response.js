df4d98c5729e5816da4dbf0c19b5bdbe3e6dda37
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-06-06 07:57:06 +0000

    Mon, Jun 6, 2022, 7:57 AM +00:00

--- public/javascripts/invite-response.js
+++ public/javascripts/invite-response.js
@@ -11,20 +11,20 @@ let timeframes = [
   }
 ]
 
-window.onload = () => {
-  var ctn = document.getElementsByClassName('response-avail-ctn')[0];
-  for (let i in timeframes) {
-    var tfDIV = document.createElement('div');
-    tfDIV.classList.add('event-input');
-    tfDIV.classList.add('response-info-tf-input');
-    tfDIV.id = i
-    tfDIV.onclick = function () {
-      this.classList.toggle("chosen-tf")
-    };
-    tfDIV.innerText = 'From ' + timeframes[i]['from'] + ' To ' + timeframes[i]['to'];
-    ctn.appendChild(tfDIV);
-  }
-}
+// window.onload = () => {
+//   var ctn = document.getElementsByClassName('response-avail-ctn')[0];
+//   for (let i in timeframes) {
+//     var tfDIV = document.createElement('div');
+//     tfDIV.classList.add('event-input');
+//     tfDIV.classList.add('response-info-tf-input');
+//     tfDIV.id = i
+//     tfDIV.onclick = function () {
+//       this.classList.toggle("chosen-tf")
+//     };
+//     tfDIV.innerText = 'From ' + timeframes[i]['from'] + ' To ' + timeframes[i]['to'];
+//     ctn.appendChild(tfDIV);
+//   }
+// }
 
 const sendTimeFrames = () => {
   let chosenTF = document.getElementsByClassName("chosen-tf");
