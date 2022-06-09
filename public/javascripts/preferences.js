3b45af0eabd4b9daf6e2a2ab444befd13c49521a
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-06-09 08:04:37 +0000

    demo run

--- public/javascripts/preferences.js
+++ public/javascripts/preferences.js
@@ -13,6 +13,7 @@ function updateEmailPreference() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        alert("Notification preference has been updated!!!")
       }
   }
 
