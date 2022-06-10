47f2248a6071d9204c55696f3155c784b5d38d27
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-06-10 12:18:14 +0000

    Fri, Jun 10, 2022, 12:18 PM +00:00

--- public/javascripts/login-signup.js
+++ public/javascripts/login-signup.js
@@ -98,7 +98,7 @@ function signup() {
   xhttp.open("POST", "/signup", true);
   xhttp.setRequestHeader("Content-type", "application/json");
   xhttp.send(JSON.stringify({ username:username , password:password , email:email}));
-};
+}
 
 function onSignIn(googleUser) {
 
@@ -118,12 +118,7 @@ function onSignIn(googleUser) {
               getUser();
               window.location = '/users/profile';
             }
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-            });
-        }// else if(this.readyState == 4 && this.status >= 400){
-        //   alert("Login Failed! Google login incorrect.");
-        // }
+        }
     };
 
     var id_token = googleUser.getAuthResponse().id_token;  //Token from Google side
@@ -137,43 +132,4 @@ function onSignIn(googleUser) {
     }));
 
     //So generally it will get a id token from Google side and send it to our server in JSON format. Just adjust anything here as we need.
-}
-
-function onSignIn(googleUser) {
-
-  //So guys extract anything you need from 'profile', just like what I shown below
-  var profile = googleUser.getBasicProfile();
-
-  let xhttp = new XMLHttpRequest();
-
-  xhttp.onreadystatechange = function() {
-
-      if(this.readyState == 4 && this.status == 200){
-          let user = JSON.parse(this.responseText)[0];
-          if (user.isAdmin) {
-            getAdmin();
-            window.location = '/admin/admin-user'
-          } else {
-            getUser();
-            window.location = '/users/profile'
-          }
-          var auth2 = gapi.auth2.getAuthInstance();
-          auth2.signOut().then(function () {
-          });
-      }// else if(this.readyState == 4 && this.status >= 400){
-      //   alert("Login Failed! Google login incorrect.");
-      // }
-  };
-
-  var id_token = googleUser.getAuthResponse().id_token;  //Token from Google side
-
-  //I dont know if u guys want to do login like this. This is just a tmp mock one to show u guys how openID work
-
-  xhttp.open("POST", "/googleLogin"); //An unique request for openID login
-  xhttp.setRequestHeader("Content-type", "application/json");
-  xhttp.send(JSON.stringify({
-    token: id_token
-  }));
-
-  //So generally it will get a id token from Google side and send it to our server in JSON format. Just adjust anything here as we need.
 }
